f0bd44cb24f5f1b757f22ecec73504e16d730e96
PlebSenator <Derlemond@gmail.com>
2020-09-20 22:56:24 +0300

    final commit for first chapter

--- ch1/ex5/check.lua
+++ ch1/ex5/check.lua
@@ -1,5 +1,5 @@
 print(type(nil) == nil)
 print(nil == nil)
 print(nil == type(nil))
-==Answer
+--Answer
 print(type(type(nil)))
---
+++ ch1/ex6/check.lua
@@ -0,0 +1,6 @@
+a = true --you could change there value 
+if (false == a) or (true == a) then
+	print("It is boolean")
+else
+	print("It is not boolean")
+end
